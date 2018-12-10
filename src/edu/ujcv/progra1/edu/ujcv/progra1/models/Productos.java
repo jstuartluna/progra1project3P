82730b7175a6b259a6e2c8e350917db2fea2449a
julioluna <Tegucigalpa93>
2018-12-10 08:08:37 -0600

    Creando clase Productos

--- src/edu/ujcv/progra1/edu/ujcv/progra1/models/Productos.java
+++ src/edu/ujcv/progra1/edu/ujcv/progra1/models/Productos.java
@@ -1,4 +1,47 @@
 package edu.ujcv.progra1.edu.ujcv.progra1.models;
 
 public class Productos {
+    private String cantidadProducto;
+    private String descripcionProducto;
+    private String codigoProducto;
+    private String precioProducto;
+
+    public Productos( String cantidadProducto, String descripcionProducto, String codigoProducto, String precioProducto) {
+       this.cantidadProducto = cantidadProducto;
+       this.descripcionProducto = descripcionProducto;
+       this.codigoProducto = codigoProducto;
+       this.precioProducto = precioProducto;
+    }
+
+    public String getCantidadProducto(){
+        return cantidadProducto;
+    }
+
+    public void setCantidadProducto(String cantidadProducto){
+        this.cantidadProducto = cantidadProducto;
+    }
+
+    public String getDescripcionProducto(){
+        return  descripcionProducto;
+    }
+
+    public void setDescripcionProducto(){
+        this.descripcionProducto = descripcionProducto;
+    }
+
+    public String getCodigoProducto(){
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(String codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(String precioProducto) {
+        this.precioProducto = precioProducto;
+    }
 }
